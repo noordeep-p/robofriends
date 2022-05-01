8806b8faa68ea067a50fd2243dab48a7a6c01c9c
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-05-01 17:55:16 -0400

    create card component to render api response

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+import "../index.css";
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <img
+          alt={this.props.username}
+          src={`https://robohash.org/${this.props.username}?200*200`}
+        />
+        <div>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.email}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
