e5c063385284b624beda505db3a9e87b07e90bbd
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-05-01 17:54:23 -0400

    create container to render all app components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Searchbox from "./components/Searchbox";
 
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
+import ErrorBoundry from "./components/ErrorBoundry";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  const [robots, setRobots] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const onSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => setRobots(users));
+  }, []);
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  return (
+    <div className="container">
+      <Searchbox searchChange={onSearchChange} />
+      <ErrorBoundry>
+        {filteredRobots.map((robot) => {
+          return (
+            <Card
+              key={robot.id}
+              name={robot.name}
+              username={robot.username}
+              email={robot.email}
+            />
+          );
+        })}
+      </ErrorBoundry>
+    </div>
+  );
 }
-
-export default App;
