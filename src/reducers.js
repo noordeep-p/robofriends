ff5704ad18f99ca86a130739febf7a187931fca5
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-05-04 15:19:44 -0400

    add reducer to handle API req

--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,37 @@
-import { CHANGE_SEARCH_FIELD } from "./constants";
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+} from "./constants";
 
-const intialState = {
+const intialStateSearch = {
   searchField: "",
 };
 
-export const searchRobots = (state = intialState, action = {}) => {
+export const searchRobots = (state = intialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return { ...state, searchField: action.payload };
+    default:
+      return state;
+  }
+};
+
+const intialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: "",
+};
+
+export const requestRobots = (state = intialStateRobots, action = {}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return { ...state, isPending: true };
+    case REQUEST_ROBOTS_SUCCESS:
+      return { ...state, robots: [...action.payload], isPending: false };
+    case REQUEST_ROBOTS_FAILED:
+      return { ...state, isPending: false };
     default:
       return state;
   }
